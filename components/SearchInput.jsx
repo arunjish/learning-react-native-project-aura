b7c631585d929e8729e7916d5414d46f986aafbb
Arun jishnu <lettertojishnu@gmail.com>
2025-05-25 15:21:21 +0530

    feat: Implement search functionality with dynamic query handling and improved UI components

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,25 +1,40 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
 import React, { useState } from "react";
 import { icons } from "../constants";
+import { usePathname, router } from "expo-router";
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
+const SearchInput = ({ initialQuery }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
+
   return (
     <View className="border-2 border-black-200 h-16 w-full px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
       <TextInput
         className="text-base text-white font-pregular mt-0.5 flex-1"
-        value={value}
-        placeholder={placeholder}
-        placeholderTextColor="#7b7b8b"
-        onChangeText={handleChangeText}
+        value={query}
+        placeholder="Search for a topic"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
       ></TextInput>
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert(
+              "Missing Query",
+              "Please inout something to search results across database"
+            );
+          }
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
         <Image
           source={icons.search}
           className="w-5 h-5"
